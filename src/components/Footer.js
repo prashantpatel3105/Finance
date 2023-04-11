dadbe5bfeaedb397ecfb47cec9c7c7de373f8c8a
Prashant Patel <124658989+prashantpatel3105@users.noreply.github.com>
2023-04-11 10:46:14 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,45 +14,7 @@ const Footer = () => {
   return (
 
     <>
-    {/* <div className='hero-image'>
-        <img style={{width:'1540px'}} src={f4}/>
-    <div className='hero-text'>
-        <div id='footrow' style={{justifyContent:'center'}} className='row'>
-            <div className='col-lg-3'>
-                <img style={{marginTop:'-500px'}} src={f1}/>
-                <h5 style={{color:'white',marginTop:'-230px'}}>Our Location</h5>
-                <p style={{color:'white',marginLeft:'-380px',textAlign:'center'}}>795 South Park, Wonderlan<br/>
-                     CA 94107, Australia</p>
-            </div>
-
-            <div className='col-lg-3'>
-                <img style={{marginTop:'-500px'}} src={f2}/>
-                <h5 style={{color:'white',marginTop:'-230px',marginLeft:'-30px'}}>Our Phones</h5>
-                <p style={{color:'white',marginLeft:'-350px',textAlign:'center'}}>+3695-548555 - Office<br/>
-                 +3695-548555 - Fax<br/></p>
-            </div>
-
-
-            <div className='col-lg-3'>
-                <img style={{marginTop:'-500px'}} src={f3}/>
-                <h5 style={{color:'white',marginTop:'-230px',marginLeft:'-30px'}}>Our Emails</h5>
-                <p style={{color:'white',marginLeft:'-350px',textAlign:'center'}}>Support@sitename.com<br/>Info@sitename.com</p>
-            </div>
-        </div>
-        <hr style={{color:'white',marginTop:'-80px',width:'1200px',marginLeft:'150px',border:"2px solid white"}}/>
-        <div id='footrow2' className='row'>
-            <div style={{width:'600px'}} className='col=md-6'>
-        <p style={{color:'white',marginLeft:'200px',width:'500px'}}>2022 © Consults. All rights reserved.</p>
-        </div>
-        <div className='col-md-6'>
-        <img style={{marginLeft:'580px'}} src={f5}/>
-        <img style={{marginLeft:'10px'}} src={f6}/>
-        <img style={{marginLeft:'10px'}} src={f7}/>
-        <img style={{marginLeft:'10px'}} src={f8}/>
-        </div>
-        </div>
-    </div>
-    </div> */}
+   
             <div className="footer-main">
          
                 <div className="container">
@@ -77,11 +39,11 @@ const Footer = () => {
                     </div>
                     <hr style={{border:'2px solid white'}}/>
                     <div style={{justifyContent:'center'}} className="row">
-                        <div className="col-lg-10">
-                            <p style={{color:'white'}}>2022 © Consults. All rights reserved.</p>
+                        <div className="col-lg-9">
+                            <p id='foot-icon' style={{color:'white'}}>2022 © Consults. All rights reserved.</p>
                         </div>
-                        <div className="col-lg-2">
-                            <div >                       
+                        <div className="col-lg-3">
+                            <div  id='foot-icon2' >                       
                          <img style={{marginLeft:'10px'}} src={f5}/>
                         <img style={{marginLeft:'10px'}} src={f6}/>
                         <img style={{marginLeft:'10px'}} src={f7}/>
