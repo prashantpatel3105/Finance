c6c7c947b8b8dc7557bb6155f996aa14a3a4e3d0
Prashant Patel <124658989+prashantpatel3105@users.noreply.github.com>
2023-04-11 10:48:16 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,16 +12,7 @@ const Home = () => {
   return (
     <div>
         <Nav/>
-        {/* <div   id='homepage' >
-        <img style={{width:'1540px',marginTop:'-150px'}} className='h1' src={h1}/>
-        <div style={{width:'100%',textAlign:'center'}} id='maintext' >
-        <div   className='text'>
-    <h3 className='hometext' style={{fontSize:"3rem",textAlign:'center'}}>We Offer Financial Wings To Let Your<br/><span style={{marginRight:'20px'}}> Dream Soar Higher!</span></h3>
-    <p style={{textAlign:'center',marginTop:'30px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero<br/> et velit interdum, ac aliquet odio mattis. </p>
-      <div style={{textAlign:'center',marginTop:'20px'}}> <button className='btnhome'><b>Consultation</b></button></div>
-    </div>
-        </div>
-        </div> */}
+  
 
           <div className="home-main">
                 <div style={{paddingTop:'330px',textAlign:'center'}} className='text-home'>        
